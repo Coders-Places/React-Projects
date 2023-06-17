7351192460690415cca6e673463d69bba1c2b8e9
Coders-Places <134509398+Coders-Places@users.noreply.github.com>
2023-06-17 16:07:53 +0500

    Add files via upload

---
+++ movies/movies/src/main/java/com/metflix/movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package com.metflix.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+	int f;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
